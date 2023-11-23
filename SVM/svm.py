d682b7944b9221b1cb60b20b1397375b851a8b8d
saidmmendez <enzo.mendez315@gmail.com>
2023-11-22 23:04:50 -0700

    Minor changes.

--- SVM/svm.py
+++ SVM/svm.py
@@ -82,12 +82,6 @@ class SVM:
         inputs = dataset.drop('label', axis=1).to_numpy()
         inputs = np.asmatrix(inputs)
         return np.sum((alphas * labels)[0,0] * inputs, axis=0)
-    
-        # # Initialize weights
-        # weights = np.zeros(len(dataset.columns) - 1)
-        # for i in range(alphas.size):
-        #     weights += inputs[i] * alphas[i] * labels[i]
-        # return weights
 
     def recover_dual_bias(self, alphas, dataset):
         # Separate the labels
@@ -98,20 +92,6 @@ class SVM:
         inputs = dataset.drop('label', axis=1).to_numpy()
         inputs = np.asmatrix(inputs)
         return np.mean(labels - np.sum((alphas * labels)[0,0] * np.dot(inputs, inputs.T)))
-    
-        # labels = dataset['label'].to_numpy()
-        # labels[labels == 0.0] = -1.0
-        # inputs = dataset.drop('label', axis=1).to_numpy()
-        # bias = 0
-        # num_nonzero = 0
-        # for i in range(len(alphas)):
-        #     if alphas[i] > 1e-11:
-        #         inner_sum = 0
-        #         for j in range(len(alphas)):
-        #             inner_sum += alphas[j] * labels[j] * np.dot(inputs[i], inputs[j])
-        #         bias += labels[i] - inner_sum
-        #         num_nonzero += 1
-        # return bias / num_nonzero
 
     def predict_dual(self, dataset, inputs, labels, alphas, bias, kernel):
         labels = np.asmatrix(labels.reshape((labels.size, 1)))
@@ -123,18 +103,6 @@ class SVM:
             else:
                 dataset.at[i, 'label'] = 0
         return dataset
-
-        # for index, dataset_row in dataset.iterrows():
-        #     row = dataset_row.tolist()[:-1]
-        #     row = np.array(row)
-        #     # Add bias to x
-        #     row = np.append(row, 1)
-        #     prediction = np.dot(weights, row)
-        #     if prediction >= 0.0:
-        #         dataset.at[index, 'label'] = 1
-        #     else:
-        #         dataset.at[index, 'label'] = 0
-        # return dataset
     
     def compute_error(self, actual_labels, predicted_labels):
         incorrect_examples = 0
@@ -188,46 +156,40 @@ def main():
             yield initial_gamma / (1 + t)
             t += 1
 
-    # # Results using primal svm
-    # for C in C_values:
-    #     primal_weights = svm.train_primal(train_dataset, 100, learning_rate_A(0.1, 1), C)
-    #     train_predicted_dataset = svm.predict_primal(train_predicted_dataset, primal_weights)
-    #     test_predicted_dataset = svm.predict_primal(test_predicted_dataset, primal_weights)
-    #     train_error = svm.compute_error(train_dataset['label'].to_numpy(), train_predicted_dataset['label'].to_numpy())
-    #     test_error = svm.compute_error(test_dataset['label'].to_numpy(), test_predicted_dataset['label'].to_numpy())
-    #     print('The training error for C =', C, 'in the primal domain is', train_error)
-    #     print('The test error for C =', C, 'in the primal domain is', test_error)
-
-    # for C in C_values:
-    #     primal_weights = svm.train_primal(train_dataset, 100, learning_rate_B(0.1), C)
-    #     train_predicted_dataset = svm.predict_primal(train_predicted_dataset, primal_weights)
-    #     test_predicted_dataset = svm.predict_primal(test_predicted_dataset, primal_weights)
-    #     train_error = svm.compute_error(train_dataset['label'].to_numpy(), train_predicted_dataset['label'].to_numpy())
-    #     test_error = svm.compute_error(test_dataset['label'].to_numpy(), test_predicted_dataset['label'].to_numpy())
-    #     print('The training error for C =', C, 'in the primal domain is', train_error)
-    #     print('The test error for C =', C, 'in the primal domain is', test_error)
-
-    # Results using dual svm
+    # Results using primal svm
     for C in C_values:
-        dual_alphas = svm.train_dual(train_dataset, C, np.dot)
-        dual_weights = svm.recover_dual_weights(dual_alphas, train_dataset)
-        dual_bias = svm.recover_dual_bias(dual_alphas, train_dataset)
-        train_predicted_dataset = svm.predict_dual(train_predicted_dataset, train_inputs, train_labels, dual_alphas, dual_bias, np.dot)
-        #test_predicted_dataset = svm.predict_dual(test_predicted_dataset, test_inputs, test_labels, dual_alphas, dual_bias, np.dot)
+        primal_weights = svm.train_primal(train_dataset, 100, learning_rate_A(0.1, 1), C)
+        train_predicted_dataset = svm.predict_primal(train_predicted_dataset, primal_weights)
+        test_predicted_dataset = svm.predict_primal(test_predicted_dataset, primal_weights)
         train_error = svm.compute_error(train_dataset['label'].to_numpy(), train_predicted_dataset['label'].to_numpy())
-        #test_error = svm.compute_error(test_dataset['label'].to_numpy(), test_predicted_dataset['label'].to_numpy())
-        print('The training error for C =', C, 'in the dual domain is', train_error)
-        #print('The test error for C =', C, 'in the dual domain is', test_error)
+        test_error = svm.compute_error(test_dataset['label'].to_numpy(), test_predicted_dataset['label'].to_numpy())
+        print('The training error for C =', C, 'in the primal domain is', train_error)
+        print('The test error for C =', C, 'in the primal domain is', test_error)
+        print('The weights vector for C =', C, 'in the primal domain is', primal_weights)
 
+    for C in C_values:
+        primal_weights = svm.train_primal(train_dataset, 100, learning_rate_B(0.1), C)
+        train_predicted_dataset = svm.predict_primal(train_predicted_dataset, primal_weights)
+        test_predicted_dataset = svm.predict_primal(test_predicted_dataset, primal_weights)
+        train_error = svm.compute_error(train_dataset['label'].to_numpy(), train_predicted_dataset['label'].to_numpy())
+        test_error = svm.compute_error(test_dataset['label'].to_numpy(), test_predicted_dataset['label'].to_numpy())
+        print('The training error for C =', C, 'in the primal domain is', train_error)
+        print('The test error for C =', C, 'in the primal domain is', test_error)
+        print('The weights vector for C =', C, 'in the primal domain is', primal_weights)
 
-        # dual_weights = np.hstack([dual_weights, np.full((dual_weights.shape[0], 1), dual_bias)])
-        # train_predicted_dataset = svm.predict_primal(train_predicted_dataset, dual_weights)
-        # test_predicted_dataset = svm.predict_primal(test_predicted_dataset, dual_weights)
-        # train_error = svm.compute_error(train_dataset['label'].to_numpy(), train_predicted_dataset['label'].to_numpy())
-        # test_error = svm.compute_error(test_dataset['label'].to_numpy(), test_predicted_dataset['label'].to_numpy())
-        # print('The training error for C =', C, 'in the dual domain is', train_error)
-        # print('The test error for C =', C, 'in the dual domain is', test_error)
-
+    # # Results using dual svm
+    # for C in C_values:
+    #     dual_alphas = svm.train_dual(train_dataset, C, np.dot)
+    #     dual_weights = svm.recover_dual_weights(dual_alphas, train_dataset)
+    #     dual_bias = svm.recover_dual_bias(dual_alphas, train_dataset)
+    #     dual_weights = np.hstack([dual_weights, np.full((dual_weights.shape[0], 1), dual_bias)])
+    #     # train_predicted_dataset = svm.predict_dual(train_predicted_dataset, train_inputs, train_labels, dual_alphas, dual_bias, np.dot)
+    #     # test_predicted_dataset = svm.predict_dual(test_predicted_dataset, test_inputs, test_labels, dual_alphas, dual_bias, np.dot)
+    #     # train_error = svm.compute_error(train_dataset['label'].to_numpy(), train_predicted_dataset['label'].to_numpy())
+    #     # test_error = svm.compute_error(test_dataset['label'].to_numpy(), test_predicted_dataset['label'].to_numpy())
+    #     # print('The training error for C =', C, 'in the dual domain is', train_error)
+    #     # print('The test error for C =', C, 'in the dual domain is', test_error)
+    #     print('The weights vector for C =', C, 'in the dual domain is', dual_weights)
 
 if __name__ == "__main__":
     main()
