6ad4009b0256eed6723db09b343e4ecd49d2079b
saidmmendez <enzo.mendez315@gmail.com>
2023-12-08 23:32:12 -0700

    Minor changes.

--- Neural Networks/neural_network.py
+++ Neural Networks/neural_network.py
@@ -11,6 +11,9 @@ class Neural_Network:
         hidden_layer1 = [{'weights': np.random.rand(number_of_inputs + 1)} for i in range(number_of_hidden_neurons)]
         hidden_layer2 = [{'weights': np.random.rand(number_of_hidden_neurons + 1)} for i in range(number_of_hidden_neurons)]
         output_layer = [{'weights': np.random.rand(number_of_hidden_neurons + 1)} for i in range(number_of_outputs)]
+        # hidden_layer1 = [{'weights': np.zeros(number_of_inputs + 1)} for i in range(number_of_hidden_neurons)]
+        # hidden_layer2 = [{'weights': np.zeros(number_of_hidden_neurons + 1)} for i in range(number_of_hidden_neurons)]
+        # output_layer = [{'weights': np.zeros(number_of_hidden_neurons + 1)} for i in range(number_of_outputs)]
         network.append(hidden_layer1)
         network.append(hidden_layer2)
         network.append(output_layer)
@@ -21,10 +24,15 @@ class Neural_Network:
         # Add bias to x
         row = np.append(row, 1)
         for layer_index, layer in enumerate(network):
-            new_inputs = []
+            outputs = []
             for neuron in layer:
-                activation = np.dot(neuron['weights'], row)
-                activation = np.clip(activation, 1e-7, 1-1e-7)
+                weights = neuron['weights']
+                # Start with the bias
+                activation = weights[-1]
+                for i in range(len(weights) - 1):
+                    # Add every input times its weight
+                    activation += weights[i] * inputs[i]
+                activation = np.clip(activation, 1e-7, 1-1e-7) #CHECK THIS LINE -----------
                 if layer_index != len(network) - 1:
                     # Using the sigmoid activation function for hidden layers
                     output = 1.0 / (1.0 + np.exp(-activation))
@@ -32,12 +40,13 @@ class Neural_Network:
                     # Using only the linear combination for output layer
                     output = activation
                 neuron['output'] = output
-                new_inputs.append(output)
-            inputs = new_inputs
+                outputs.append(output)
+            inputs = outputs
         # Return the output from the last layer
-        return inputs
+        return inputs[0]
     
     def compute_backpropagation(self, network, actual_label):
+        # Start from output layer and work your way down
         for layer_index in reversed(range(len(network))):
             layer = network[layer_index]
             losses = []
@@ -46,7 +55,7 @@ class Neural_Network:
                 for neuron_index in range(len(layer)):
                     loss = 0.0
                     for previous_neuron in network[layer_index + 1]:
-                        # Add up loss from previous layer
+                        # Add up the loss from previous layer: weight times loss of previous neuron
                         loss += previous_neuron['weights'][neuron_index] * previous_neuron['loss']
                         losses.append(loss)
             # For the output layer only
@@ -56,8 +65,12 @@ class Neural_Network:
                     losses.append(predicted_label - actual_label)
             for neuron_index, neuron in enumerate(layer):
                 neuron_output = neuron['output']
-                # Compute loss using the derivative of the sigmoid function
-                neuron['loss'] = losses[neuron_index] * neuron_output * (1.0 - neuron_output)
+                if layer_index != len(network) - 1:
+                    # Compute loss using the derivative of the sigmoid function for hidden layers
+                    neuron['loss'] = losses[neuron_index] * neuron_output * (1.0 - neuron_output)
+                else:
+                    # Compute loss using the derivative of MSE for output layer
+                    neuron['loss'] = losses[neuron_index]
 
     def train(self, network, train_dataset, learning_rate, epochs=100):
         for epoch in range(epochs):
@@ -65,8 +78,10 @@ class Neural_Network:
             train_dataset = train_dataset.sample(frac=1.0)
             for index, dataset_row in train_dataset.iterrows():
                 actual_label = dataset_row['label']
+                if actual_label == 0.0:
+                    actual_label = -1.0
                 row = dataset_row.tolist()[:-1]
-                predicted_label = self.compute_forward_pass(network, row)
+                self.compute_forward_pass(network, row)
                 self.compute_backpropagation(network, actual_label)
                 gamma = next(learning_rate)
                 for layer_index, layer in enumerate(network):
@@ -81,8 +96,23 @@ class Neural_Network:
                         # Update the bias
                         neuron['weights'][-1] -= gamma * neuron['loss']
 
-    def predict(self):
-        pass
+    def predict(self, network, dataset):
+        for index, dataset_row in dataset.iterrows():
+            row = dataset_row.tolist()[:-1]
+            predicted_label = self.compute_forward_pass(network, row)
+            if np.sign(predicted_label) == -1.0:
+                predicted_label = 0
+            else:
+                predicted_label = 1
+            dataset.at[index, 'label'] = predicted_label
+        return dataset
+    
+    def compute_error(self, actual_labels, predicted_labels):
+        incorrect_examples = 0
+        for i in range(len(actual_labels)):
+            if actual_labels[i] != predicted_labels[i]:
+                incorrect_examples += 1
+        return incorrect_examples / len(actual_labels)
 
 def main():
     # Get the directory of the script
@@ -106,13 +136,6 @@ def main():
     train_predicted_dataset['label'] = ""   # or = np.nan for numerical columns
     test_predicted_dataset = pd.DataFrame(test_dataset)
     test_predicted_dataset['label'] = ""   # or = np.nan for numerical columns
-        # Create training and test arrays
-    train_inputs = train_dataset.drop('label', axis=1).to_numpy()
-    train_labels = train_dataset['label'].to_numpy().copy()
-    train_labels[train_labels == 0.0] = -1.0
-    test_inputs = test_dataset.drop('label', axis=1).to_numpy()
-    test_labels = test_dataset['label'].to_numpy().copy()
-    test_labels[test_labels == 0.0] = -1.0
 
     def learning_rate(initial_gamma, alpha):
         t = 0
@@ -120,23 +143,26 @@ def main():
             yield initial_gamma / (1 + (initial_gamma / alpha) * t)
             t += 1
 
-    row = train_dataset.drop('label', axis=1).sample(n=1).to_numpy()
-    network = nn.create_network()
-    print(network)
-    nn.train(network, train_dataset, learning_rate(0.1, 1))
-    print()
-    # final_output = nn.compute_forward_pass(network, row)
-    # print('The network is', network)
-    # print()
-    # nn.compute_backpropagation(network, train_dataset['label'].sample(n=1).iloc[0])
+    width_values = [5, 10, 25, 50, 100]
 
-    print('The network is', network)
-    # print()
-    # print('The neurons outputs are', outputs)
-    # print()
-    # print('The neurons losses are', losses)
-    # print()
-    # print('The final output is', final_output)
+    for width in width_values:
+        network = nn.create_network(number_of_hidden_neurons=width)
+        nn.train(network, train_dataset, learning_rate(0.1, 1))
+        train_predicted_dataset = nn.predict(network, train_predicted_dataset)
+        test_predicted_dataset = nn.predict(network, test_predicted_dataset)
+        train_error = nn.compute_error(train_dataset['label'].to_numpy(), train_predicted_dataset['label'].to_numpy())
+        test_error = nn.compute_error(test_dataset['label'].to_numpy(), test_predicted_dataset['label'].to_numpy())
+        print('The training error for width =', width, 'is', train_error)
+        print('The test error for width =', width, 'is', test_error)
+
+    # network = nn.create_network(number_of_hidden_neurons=6)
+    # nn.train(network, train_dataset, learning_rate(0.1, 1))
+    # train_predicted_dataset = nn.predict(network, train_predicted_dataset)
+    # test_predicted_dataset = nn.predict(network, test_predicted_dataset)
+    # train_error = nn.compute_error(train_dataset['label'].to_numpy(), train_predicted_dataset['label'].to_numpy())
+    # test_error = nn.compute_error(test_dataset['label'].to_numpy(), test_predicted_dataset['label'].to_numpy())
+    # print('The training error for width =', train_error)
+    # print('The test error for width =', test_error)
 
 if __name__ == "__main__":
     main()
